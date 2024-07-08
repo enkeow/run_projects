245ce8b698850c54948467790d08b1b36fa58b2a
Mariami Vinogradova <elenaa745@gmail.com>
2024-07-08 09:35:48 +0300

    main

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from webapp.templates import main_page, index, main
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def main_page():
+    return render_template("main_page.html")
+
+@app.route("/article")
+def article():
+    # db.session
+    return render_template("article.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
