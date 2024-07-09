583be69e2e15a99a5d0d82379897ef344611fd67
Mariami Vinogradova <elenaa745@gmail.com>
2024-07-09 19:32:39 +0300

    migrare

--- add_article.py
+++ add_article.py
@@ -2,6 +2,6 @@ from db import db_session
 from model import Article_run
 
 
-first_article = Article_run(title = 'qwe', text = 'ashhjjhvdfghj')
+first_article = Article_run(title='qwe', text='ashhjjhvdfghj')
 db_session.add(first_article)
 db_session.commit()
--- db.py
+++ db.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-  
 
 engine = create_engine('postgresql://postgres:Konako325@localhost:5432/postgres')
 db_session = scoped_session(sessionmaker(bind=engine))
