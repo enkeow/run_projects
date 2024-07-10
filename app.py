009e65387ddeab097bf82b0c7f0da0c05d107b6e
Mariami Vinogradova <elenaa745@gmail.com>
2024-07-10 21:51:19 +0300

    Добавила forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from model import (
     db,
     User,
@@ -11,8 +11,10 @@ from model import (
 from flask_migrate import Migrate
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField 
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
+
+from forms import ArticleForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Konako325@localhost:5432/postgres'
@@ -27,21 +29,26 @@ def main_page():
 
 @app.route("/article")
 def article():
-    # db.session
+    db.session
     return render_template("article.html")
 
 
 @app.route("/article", methods=["POST"])
 def add_article():
-    # form = LoginForm()
-    #     if form.validate_on_submit():
-    # article = Article_run(
-    #     title=title,
-    #     text=text
-    # )
-    # db.session.add(article)
-    # db.session.commit()
-    pass
+    form = ArticleForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        article= Article_run(
+            title=title,
+            text=text
+        )
+        db.session.add(article)
+        db.session.commit()
+        flash('Статья успешно добавлена на сайт')
+        return render_template('articles.html', form=form)
+    return render_template('add_article.html', form=form)
+
 
 
 if __name__ == "__main__":
---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class ArticleForm(FlaskForm):
+    title = StringField(
+        'Заголовок статьи',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+    )
+    text = StringField(
+        'Текст статьи',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+    )
+    submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary"})
