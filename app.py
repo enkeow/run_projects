6e98c4615586501c01c95a7654b768367dfd3fda
Mariami Vinogradova <elenaa745@gmail.com>
2024-07-17 21:50:40 +0300

    + add_race, races

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
-from forms import LoginForm
+from forms import LoginForm, RaceForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Konako325@localhost:5432/postgres'
 db.init_app(app)
 migrate = Migrate(app, db)
 
-from model import Article_run
+from model import Article_run, Race
 app.secret_key = 'dovlatov-chemodan'
 
 
@@ -30,7 +30,7 @@ def main_page():
     return render_template("main_page.html")
 
 
-@app.route("/add_article", methods=["POST", "GET"])
+@app.route("/add_article", methods=["GET", "POST"])
 def add_article():
     form = LoginForm()
     if request.method == "GET":
@@ -65,6 +65,34 @@ def article_id(article_id):
 def club_run():
     club = Club_run.query.all()
     return render_template('Club_run.html', club=club)
+
+
+@app.route('/add_race', methods=['POST', 'GET'])
+def add_race():
+    form = RaceForm()
+    if request.method == 'GET':
+        return render_template('add_race.html', form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            race = Race(name=form.name.data,
+                        distance=form.distance.data,
+                        date=form.date.data,
+                        location=form.location.data,
+                        info=form.info.data)
+            db.session.add(race)
+            db.session.commit()
+            flash('Забег успешно добавлен!')
+            return redirect(url_for('main_page'))
+        else:
+            return render_template('add_race.html', form=form)
+
+
+
+
+@app.route('/races/')
+def races_page():
+    races = Race.query.order_by(Race.id.desc()).all()
+    return render_template('races_page.html', races=races)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, IntegerField, DateField, FloatField
 from wtforms.validators import DataRequired
 
 
@@ -15,3 +15,37 @@ class LoginForm(FlaskForm):
         render_kw={"class": "form-control text", "rows": 5}
     )
     submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary"})
+
+class RaceForm(FlaskForm):
+    name = StringField(
+        'Название забега',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+        
+    )
+    distance = FloatField(
+        'Дистанция (в км.)',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+        
+    )
+    date = DateField(
+        'Дата забега',
+        format='%Y-%m-%d',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+    )
+    location = StringField(
+        'Где будет проходить забег',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+    )
+    info = TextAreaField(
+        'Информация о забеге',
+        validators=[DataRequired()],
+        render_kw={"class": "form-control", "rows": 5}
+    )
+    submit = SubmitField(
+        'Добавить забег', render_kw={"class": "btn btn-primary"}
+    )
+
